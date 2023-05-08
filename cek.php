073be7431ea07650b256c414b5e1435d20c24a53
milkhamil <32932970+milkhamil@users.noreply.github.com>
2023-05-09 03:49:31 +0700

    Update cek.php

--- cek.php
+++ cek.php
@@ -94,7 +94,7 @@ function create_response($text, $message)
     $lokalformat = $input;
     $interformat = "+".$input;
     $rawformat = "0".$caknomer[2].$caknomer[3].$caknomer[4].$caknomer[5].$caknomer[6].$caknomer[7].$caknomer[8].$caknomer[9].$caknomer[10].$caknomer[11].$caknomer[12].$caknomer[13];
-    $spasiformat = "0".$caknomer[2].$caknomer[3].$caknomer[4]." ".$caknomer[5].$caknomer[6].$caknomer[7].$caknomer[8]." ".$caknomer[9].$caknomer[10].$caknomer[11].$caknomer[12].$caknomer[13];
+    $spasiformat = "0".$caknomer[2].$caknomer[3].$caknomer[4]."+".$caknomer[5].$caknomer[6].$caknomer[7].$caknomer[8]."+".$caknomer[9].$caknomer[10].$caknomer[11].$caknomer[12].$caknomer[13];
     $garisformat = "0".$caknomer[2].$caknomer[3].$caknomer[4]."-".$caknomer[5].$caknomer[6].$caknomer[7].$caknomer[8]."-".$caknomer[9].$caknomer[10].$caknomer[11].$caknomer[12].$caknomer[13];
     $phogageneral1 = ("https://www.google.com/search?q=intext%3A%22".$lokalformat."%22+OR+intext%3A%22%2B".$lokalformat."%22+OR+intext%3A%22".$rawformat."%22+OR+intext%3A%22".$spasiformat."%22+OR+intext%3A%22".$garisformat."%22");
 	
